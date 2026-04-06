a1fbf083f653ef923324667db2e8b8ca6429d413
pcrab <pcrab@pcrab.xyz>
2026-04-06 19:04:28 +0800

    remove wrangler

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,8 @@ import react from "@vitejs/plugin-react";
 import UnoCSS from "unocss/vite";
 import { defineConfig } from "vite";
 
-import { cloudflare } from "@cloudflare/vite-plugin";
-
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), UnoCSS(), cloudflare()],
+	plugins: [react(), UnoCSS()],
 });
+
